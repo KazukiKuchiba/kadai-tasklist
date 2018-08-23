771f6f9b1f082029d200eb51db3c2a058c2d72db
kazuki.kuchiba <revolutionno_8@yahoo.co.jp>
2018-08-23 12:39:15 +0000

    bootstrap repaired

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -5,11 +5,8 @@
     <h1>タスク新規作成ページ</h1>
     
     <div class="row">
-        <div class="col-xs-12">
-        <div class="col-sm-offset-2">
-        <div class="col-sm-8">
-        <div class="col-lg-offset-3">
-        <div class="col-lg-6">
+        <div class="col-xs-12 col-sm-offset-2 col-sm-8 col-md-8 col-md-offset-2 col-lg-offset-3 col-lg-6">
+       
             {!! Form::model($task, ['route' => 'tasks.store']) !!}
                 
                 <div class="form-group">
@@ -25,10 +22,6 @@
             {!! Form::submit('投稿', ['class' => 'btn btn-primary']) !!}
         
             {!! Form::close() !!}
-        </div>
-        </div>
-        </div>
-        </div>
         </div>
     </div>
 @endsection
--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -5,11 +5,8 @@
     <h1>id: {{ $task->id }} のタスク編集ページ</h1>
     
     <div class="row">
-        <div class="col-xs-12">
-        <div class="col-sm-offset-2">
-        <div class="col-sm-8">
-        <div class="col-lg-offset-3">
-        <div class="col-lg-6">
+        <div class="col-xs-12 col-sm-offset-2 col-sm-8 col-md-8 col-md-offset-2 col-lg-offset-3 col-lg-6">
+            
             {!! Form::model($task, ['route' => ['tasks.update', $task->id], 'method' => 'put']) !!}
                 
                 <div class="form-group">
@@ -25,10 +22,6 @@
                 {!! Form::submit('更新', ['class' => 'btn btn-default']) !!}
         
             {!! Form::close() !!}
-        </div>
-        </div>
-        </div>
-        </div>
         </div>
     </div>
 @endsection
